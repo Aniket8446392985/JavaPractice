4ee99e44335b9f4b1e05b3fd0a511444cc093ea8
ANIKET8446392985 <aniketgauri.dhawale@gmail.com>
2023-11-23 09:04:56 +0530

    Arrays class added

---
+++ src/Arrays.java
@@ -0,0 +1,7 @@
+public class Arrays {
+    public static void main(String args[]) {
+        String [] Aniket= {"aniket", "ankush", "Dhawale"};
+        String firstname=Aniket[0];
+        System.out.println(firstname);
+    }
+}

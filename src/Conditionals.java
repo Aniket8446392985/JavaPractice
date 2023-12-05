f87f0405896d7bc1c972946d777dee4e8ceaa760
ANIKET8446392985 <aniketgauri.dhawale@gmail.com>
2023-12-05 14:53:45 +0530

    if else added

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class Conditionals {
+
+    public void CheckDriveAge(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your age");
+        int Age = sc.nextInt();
+        if (Age >= 18) {
+            System.out.println("you can drive");
+        } else {
+            System.out.println("NOT drive");
+        }
+    }
+    public static void main(String args[])
+    {
+        Conditionals cd= new Conditionals();
+        cd.CheckDriveAge();
+    }
 }
+
+
+

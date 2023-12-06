5d831bfc49649d6c37d73a35f017e9bdf910062f
ANIKET8446392985 <aniketgauri.dhawale@gmail.com>
2023-12-06 12:38:17 +0530

    if else if laddre added

--- src/Conditionals_IfElse.java
+++ src/Conditionals_IfElse.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
 
-public class Conditionals {
+public class Conditionals_IfElse {
     public static void main (String args[])
     {
         // If Else stataments
         // if the condition written inside the if statement (condition) is true then if block will execute or else block will execute
 
         Scanner sc = new Scanner(System.in);
-/*        System.out.println("Please enter your age");
+        System.out.println("Please enter your age");
         int Age = sc.nextInt();
 
-       // relational operator
+        // relational operator
+        // == , >= , <= , != are the relational operators and can be used in if else statement condition
         if (Age >= 18) {
             System.out.println("you can drive");
         } else {
             System.out.println("NOT drive");
         }
-*/
+
         // Logical operators
+        // Logical operators are &&--and, ||--or , !--Not
+        // True && True = True --- rest all combinations are false
+        // False || False = False --- Rest all combination are true
+
         System.out.println("Please enter the name ");
         String Name= sc.nextLine();
         System.out.println("Please enter the text to match with name");
@@ -30,7 +35,6 @@ public class Conditionals {
         else {
             System.out.println("Both name are different");
         }
-
 
     }
 }

6c005dfc4976e884e6e9194f64a9b31b97429899
ANIKET8446392985 <aniketgauri.dhawale@gmail.com>
2023-12-18 10:07:52 +0530

    Multi dim array study added

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -24,13 +24,13 @@ public class ArrayPractice {
         numbers[4]=50;
       //  numbers[5]=60;
 
-
         // 3rd way
 
         int [] third={1,2,3,4,5};
         // System.out.println(third.length);
 
-
+        // For each loop it is used to iterate the complete array.
+        // for tis we have to define a new variale i inside the for loop
         for ( int i :third) {
            System.out.println(i);
         }
